8a8576d1bc6a564487ac9a0ebc29dcf2c826d062
shsioi <dragonbass49@gmail.com>
2026-02-17 22:49:48 +0700

    Add files via upload

---
+++ projectcomp.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+#include<iomanip>
+
+using namespace std;
+//หมายเหตุ ส่วนcode ที่ถูกcommentคือส่วนที่ยังไม่มั่นใจว่าจะจัดการอย่างไร
+
+
+
+
+/*string difRooM[5]=("first","second","third","fourth","last");
+fuction setmon
+
+class Unit{
+		string name;
+		string type;		
+		double hp;
+		double hpmax;
+		double atk;
+	    double def;
+        int spd;
+         bool guard_on;
+        double hppart_head ///array 5ส่วน
+        double 
+				
+	public:			
+		void create(string);//อาจจะไม่ใช้
+		void showStatus();
+		void personTurn();
+		double attack(Unit &);
+		double beAttacked(int);
+		double heal();
+        double buffatk;
+        double buffdef;	
+		//void guard();
+		bool isDead();	
+};
+*/
+
+/*class action{
+
+
+
+}//Unit จะเลือก opp หรือ เพื่อนร่วมทีม ที่จะใช้actionใส่
+*/
+
+
+
+
+
+int main(){
+    srand(time(0));
+    Unit ;// mancha mon 
+    
+    //loopถาม ผูัเล่นเลือกห้อง ถ้าไม่ผ่านห้องก่อนหน้าเลือกห้องกัดไปไม่ได้//room >turn >unit spd
+    /*Room (int ,int array ,int array   )*/
+    //array ตัวแรกเอาไว้เก็บว่ามอนจะมีตัวอะไรบ้าง array ตัวที่2skipturn มอนที่ไม่อยู่ในด่านนั้นๆ
+    /*
+    
+    turn_num = 0 ;
+         i++
+
+    //i++ไปเลื่อยๆ ถ้า mancha หรือ mon == false หยุด กลับไปเลือกห้อง
+        spd mancha ++
+        turn_num % 100- Unit spd == 0
+        //ถ้าหารลง ทำ action (
+        bool turn_mancha
+        bool turn_mon
+     (ถ้าUnit หารลงหลายUnitสุ่ม() ทุกครั้งที่ turn Unit ถูก set เป็น true N++
+     
+     )
+     set N ใน loop N==Unitที่    
+    while (set for loop turn = true){
+        for(int i;i<N:i++)(
+        [function action]
+        
+        )// loop ทำ action
+    
+    }  
+    */
+
+}
